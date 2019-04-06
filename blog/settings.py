9804d8880dfb9a155d9c513387d33cef699d0f89
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2019-04-06 11:30:56 +0200

    Update settings

--- blog/settings.py
+++ blog/settings.py
@@ -130,11 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static_in_env')
+STATIC_ROOT = STATICFILES_DIRS
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Tinymce
 
